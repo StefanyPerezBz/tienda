b63d84b1ef81e732f3305ac471062ec2a8c5b5ed
StefanyPerezBz <stefanyperezbazan@gmail.com>
2024-10-05 00:13:25 -0500

    primera version

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home.home');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
